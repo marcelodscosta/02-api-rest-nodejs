028f38e2959c513cb4b49f51a68500dcc29a7581
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-05-01 20:59:43 -0300

    installing and configuring eslint

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/hello', () => {
-  return 'Hello Wo';
-});
-
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP server running');
-  
+  return 'Hello Wo'
 })
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running')
+  })
