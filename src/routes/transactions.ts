9d4220f32a25224380de3884cf4e0fa9801cca57
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-05-09 21:28:27 -0300

    searching transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,25 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export const transactionsRoutes = async (app: FastifyInstance) => {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = knex('transactions').where('id', id).first()
+
+    return transaction
+  })
+
   app.post('/', async (request, reply) => {
     // { title, amount, type: credit or debit}
 
